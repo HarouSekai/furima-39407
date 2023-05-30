b1f2f163137703f74336e2e42c51685081c0055e
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-05-30 23:39:39 +0900

    spec/factories/users.rbの編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user do   
+    nickname                  {Faker::Name.initials}
+    email                     {Faker::Internet.free_email}
+    password                  {Faker::Internet.password}
+    password_confirmation     {password}
+    last_name                 {Faker::Name.last_name}
+    first_name                {Faker::Name.first_name}
+    last_name_pronunciation   {"ラストネーム"}
+    first_name_pronunciation  {"ファーストネーム"}
+    birthday                  {"2000-10-20"} 
   end
 end
