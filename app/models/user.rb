5c4f40b95f74666204ff3aae7401106faf583810
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-05-30 23:50:53 +0900

    お名前、お名前カナにバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
   validates :nickname,                 presence: true
-  validates :last_name,                presence: true
-  validates :first_name,               presence: true
-  validates :last_name_pronunciation,  presence: true
-  validates :first_name_pronunciation, presence: true
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/} do
+    validates :last_name_pronunciation
+    validates :first_name_pronunciation
+  end
   validates :birthday,                 presence: true
 end
