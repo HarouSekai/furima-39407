e8b169849051f260718685fe972fe7e83930202f
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-05-31 18:45:26 +0900

    userモデル単体テストコードの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,106 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe '新規登録' do
+    context '新規登録できる場合' do
+      it '入力が正しければ登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録できない場合' do
+      it 'nicknameがなければ登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
+      it 'emailがなければ登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it '重複するemailは登録できない' do
+        another_user = FactoryBot.create(:user)
+        @user.email = another_user.email
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email has already been taken")
+      end
+
+      it 'emailは@を含まなければ登録できない' do
+        @user.email = "email"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+
+      it 'passwordがなければ登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordが6文字未満では登録できない' do
+        @user.password = "abc12"
+        @user.password_confirmation = "abc12"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+
+      it 'passwordが129文字以上では登録できない' do
+        @user.password = Faker::Internet.password(min_length: 129, max_length: 150)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
+      end
+
+      it 'passwordが半角英数字混合でなければ登録できない' do
+        @user.password = "123456"
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+
+      it 'passwordとpassword_confirmationが一致しなければ登録できない' do
+        @user.password = "123abc"
+        @user.password_confirmation = "abc123"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it 'last_nameは全角（漢字・ひらがな・カタカナ）でなければ登録できない' do
+        @user.last_name = 'ﾔﾏﾀﾞ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
+
+      it 'first_nameは全角（漢字・ひらがな・カタカナ）でなければ登録できない' do
+        @user.first_name = 'Tarou'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
+      end
+
+      it 'last_name_pronunciationは全角（カタカナ）でなければ登録できない' do
+        @user.last_name_pronunciation = 'やまだ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name pronunciation is invalid")
+      end
+
+      it 'first_name_pronunciationは全角（カタカナ）でなければ登録できない' do
+        @user.first_name_pronunciation = 'ﾀﾛｳ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name pronunciation is invalid")
+      end
+
+      it 'birthdayがなければ登録できない' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+    end
+  end
 end
