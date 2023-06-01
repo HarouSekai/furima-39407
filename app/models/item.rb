5cd46682315d777839b28583cddb04c722b3be75
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-01 21:05:21 +0900

    ActiveHashに関するエラーメッセージを変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,8 @@ class Item < ApplicationRecord
   belongs_to :waiting_days_until_shipment
   belongs_to :user
   validates :name, :outline, :user_id, presence: true
-  validates :category_id, :state_id, :postage_id, :prefecture_id, :waiting_days_until_shipment_id, presence: true, numericality: {other_than: 1}
+  validates :category_id, :state_id, :postage_id, :prefecture_id, :waiting_days_until_shipment_id,
+            presence: true, numericality: {other_than: 1, message: "can't be blank"}
   with_options presence: true, numericality: {in: 300..9999999}, format: {with: /\A[\d]+\z/} do
     validates :price
   end
