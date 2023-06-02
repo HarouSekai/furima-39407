d59ee16d19136c5d7358f3bf896e79e40a52520f
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-02 10:38:55 +0900

    FactoryBotでitemモデルのインスタンスを生成可能にする

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,13 @@ FactoryBot.define do
   factory :item do
     name { "MyString" }
     outline { "MyText" }
-    category_id { 1 }
-    state_id { 1 }
-    postage_id { 1 }
-    prefecture_id { 1 }
-    waiting_days_until_shipment_id { 1 }
-    price { 1 }
-    user { nil }
+    category_id { 2 }
+    state_id { 2 }
+    postage_id { 2 }
+    prefecture_id { 2 }
+    waiting_days_until_shipment_id { 2 }
+    price { 300 }
+    
+    association :user
   end
 end
