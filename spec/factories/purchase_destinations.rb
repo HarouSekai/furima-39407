e641f52100d4c43b2b99cfcf2ab175662a7bc110
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-05 22:49:24 +0900

    FactoryBotを設定

--- spec/factories/purchase_destinations.rb
+++ spec/factories/purchase_destinations.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchase_destination do
-    
+    post_code { '100-0001' }
+    prefecture_id { 14 }
+    municipality { '千代田区' }
+    address { '千代田１－１' }
+    building { '皇居' }
+    telephone_number { '0312345678' }
   end
 end
