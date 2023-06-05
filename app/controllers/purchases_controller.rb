e5c2f4fbd5121496a6df330e716a81d2cdd00bf5
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-05 20:48:21 +0900

    createアクションの中身を定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,27 @@
 class PurchasesController < ApplicationController
+  before_action :find_item
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_destination = PurchaseDestination.new
   end
 
   def create
+    @purchase_destination = PurchaseDestination.new(purchase_destination_params)
+    if @purchase_destination.valid?
+      @purchase_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
   
+  private
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def purchase_destination_params
+    params.require(:purchase_destination).permit(:post_code, :prefecture_id, :municipality, :address, :building, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
