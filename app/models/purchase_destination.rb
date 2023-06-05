3baaf72ae6f49f8817b1d615a73315cbabf64c26
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-05 17:34:16 +0900

    バリデーションを設定

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -1,4 +1,8 @@
 class PurchaseDestination
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building, :telephone_number, :purchase_id
+
+  with_options presence: true do
+    validates :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :telephone_number, :purchase_id
+  end
 end
