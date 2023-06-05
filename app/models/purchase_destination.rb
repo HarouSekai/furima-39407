513eeb724f2e56e0bf8621a9a07bd3cf5527b53b
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-05 21:10:26 +0900

    purchase_idのバリデーションを削除

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -3,11 +3,11 @@ class PurchaseDestination
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building, :telephone_number, :purchase_id
 
   with_options presence: true do
-    validates :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :telephone_number, :purchase_id
+    validates :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :telephone_number
   end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Destination.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, telephone_number: telephone_number, purchase: purchase.id)
+    Destination.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end
