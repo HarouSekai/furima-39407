2ca853f7c927f5ef7ae1ffc556f590811e409fe4
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-05 17:59:28 +0900

    データをテーブルに保存する処理の定義

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -5,4 +5,9 @@ class PurchaseDestination
   with_options presence: true do
     validates :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :telephone_number, :purchase_id
   end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Destination.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, telephone_number: telephone_number, purchase: purchase.id)
+  end
 end
