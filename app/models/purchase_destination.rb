b664053022eb548a54426537c95a14f2175f4b40
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-06 22:34:43 +0900

    tokenにバリデーションを設定

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -4,6 +4,7 @@ class PurchaseDestination
 
   with_options presence: true do
     validates :user_id, :item_id
+    validates :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :municipality, :address
